628aa6d40b89bac704cf635814987c7b53f95f85
Guilherme-Santos131 <guilhermesilvasantos131@gmail.com>
2026-06-08 08:30:00 -0300

    criação do menu pause incompleto

---
+++ estados/menupause.py
@@ -0,0 +1,63 @@
+import pygame
+from .estado_base import EstadoBase
+
+class MenuPause(EstadoBase):
+    def __init__(self):
+        super().__init__()
+        # Inicializa o sistema de fontes do Pygame
+        pygame.font.init()
+        # Define a fonte (Nome da fonte ou None para padrão, Tamanho)
+        self.fonte = pygame.font.SysFont(None, 40)
+        self.resume = pygame.Rect(250, 50, 250, 100)
+        self.resume.center = (400, 100)
+        self.options = pygame.Rect(250, 30, 250, 100)
+        self.options.center = (400, 300)
+        self.voltamenu = pygame.Rect(250, 10, 250, 100)
+        self.voltamenu.center = (400, 500)
+        self.pause = True
+    def tratarEventos(self, lista_eventos):
+        # 1. Pega a posição do mouse uma única vez no frame
+        mx, my = pygame.mouse.get_pos()
+        click = False
+        
+        # 2. Varre a lista de eventos que o Gerenciador passou
+        for event in lista_eventos:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: # Clique com botão esquerdo
+                    click = True 
+        
+        # 3. Se houve clique em qualquer momento do frame, checa as colisões
+        if click:
+            if self.resume.collidepoint((mx, my)):
+                self.pause = False
+                pass
+            elif self.options.collidepoint((mx, my)):
+                pass
+            elif self.voltamenu.collidepoint((mx, my)):
+                self.proximo_estado = "Menuprincipal"
+                self.concluido = True
+                
+
+    def desenhar(self, tela):
+        if self.pause == True:
+            # Pinta o fundo
+            tela.fill((40, 40, 50))
+            # Desenha os retângulos dos botões
+            pygame.draw.rect(tela, 'gray', self.resume)
+            pygame.draw.rect(tela, 'gray', self.options)
+            pygame.draw.rect(tela, 'gray', self.voltamenu)
+            
+            # 1. Renderiza os textos (Texto, Antialiasing, Cor do Texto)
+            txt_resume = self.fonte.render("Resume", True, (0, 0, 0))
+            txt_options = self.fonte.render("Opções", True, (0, 0, 0))
+            txt_voltamenu = self.fonte.render("Sair", True, (0, 0, 0))
+            
+            # 2. Cria retângulos para os textos e centraliza nos botões
+            rect_txt_resume = txt_resume.get_rect(center= self.resume.center)
+            rect_txt_options = txt_options.get_rect(center= self.options.center)
+            rect_txt_voltamenu = txt_voltamenu.get_rect(center= self.voltamenu.center)
+            
+            # 3. Desenha os textos na tela
+            tela.blit(txt_resume, rect_txt_resume)
+            tela.blit(txt_options, rect_txt_options)
+            tela.blit(txt_voltamenu, rect_txt_voltamenu)
--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from estados.estado_template import Exemplo
 from estados.estado_saves import Saves
 from estados.estado_NG import NewGame
 from estados.estado_classes import Classes
+from estados.menupause import MenuPause
 def main():
     dicionarioEstados = {  # Cria um dicionário para mapear nomes em texto para as telas reais.
         "Exemplo": Exemplo(),# Instancia a tela de exemplo sob a etiqueta "Exemplo".
         "Menuprincipal": MenuPrincipal(),  
         "Saves": Saves(),
         "Newgame": NewGame(),
-        "Classes": Classes()
+        "Classes": Classes(),
+        "Menupause": MenuPause()
     }
 
     jogo = GerenciadorJogo(800,600, "Nome do jogo", dicionarioEstados, "Menuprincipal") #Cria a instância principal do jogo largura (800 px), altura (600 px), título da janela, o dicionário de telas e qual tela começa ativa.
